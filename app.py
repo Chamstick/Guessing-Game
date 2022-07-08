906c41d93fb495fd8079a22086d9895ec07696c0
Jacob Zufall <jacobzufall@gmail.com>
2022-07-07 20:16:22 -0400

    Added app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+# Python guessing game project.
+
+# Import here.
+import time
+import random
+
+# Declare variables here.
+numberAnswer = random.randint(1, 10)
+numberOfGuesses = 0
+
+# This is the code of the actual game.
+print("Welcome to the guessing game!")
+time.sleep(1)
+print("Your goal is to guess a number between 1 and 10 in three guesses.")
+time.sleep(1)
